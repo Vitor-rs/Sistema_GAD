3a9a7885d1f615a602e9443f6455fb905d32dd1f
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2025-05-27 23:27:52 -0300

    Adiciona configuração inicial do projeto com Spring Boot, incluindo arquivos de configuração, estrutura de diretórios e dependências no pom.xml.

---
+++ src/test/java/com/ifms/gad/GadApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ifms.gad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
+		// No assertions needed as the test would fail if context couldn't start
+	}
+
+}
