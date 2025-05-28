9bdbb62ac2d411978077045163317955403a1992
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2025-05-27 23:47:19 -0300

    Adiciona dependência do MapStruct e configurações do Maven Compiler Plugin; cria a classe Estudante com anotações JPA e Lombok.

--- src/test/java/com/ifms/gad/GadApplicationTests.java
+++ src/test/java/com/ifms/gad/GadApplicationTests.java
@@ -8,8 +8,7 @@ class GadApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// This test verifies that the Spring application context loads successfully
-		// No assertions needed as the test would fail if context couldn't start
+		
 	}
 
 }
